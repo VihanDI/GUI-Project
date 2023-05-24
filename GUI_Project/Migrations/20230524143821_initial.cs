5b588ee9aa735a0661f4ee8020a5139ba721cc3b
Inupa Vihan <inupavihan9@gmail.com>
2023-05-24 23:18:38 +0530

    Phase 09
    Added
    -Business logic to calculate the subtotal
    -Subtotal display
    -Unit price property to the Transaction class
    -Unit price column to the List of Transactions table in the database

---
+++ GUI_Project/Migrations/20230524143821_initial.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GUI_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListofProducts",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListofProducts", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListofTransactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Product = table.Column<string>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    UnitPrice = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListofTransactions", x => x.TransactionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListofUsers",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListofUsers", x => x.UserID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListofProducts");
+
+            migrationBuilder.DropTable(
+                name: "ListofTransactions");
+
+            migrationBuilder.DropTable(
+                name: "ListofUsers");
+        }
+    }
+}
